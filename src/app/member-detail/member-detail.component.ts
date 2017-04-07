bd6658c86620b5db21e854fb36defc4a9b7f667a
ReneeMei <meiqianye@gmail.com>
2017-04-07 10:31:58 -0700

    make the detail page functional

--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../member.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2'
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  providers: [MemberService]
 })
 export class MemberDetailComponent implements OnInit {
+  memberToDisplay;
+  memberId: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private memberService: MemberService ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.memberId = urlParameters['id'];
+    });
+    this.memberToDisplay = this.memberService.getMemberById(this.memberId);
   }
 
 }
